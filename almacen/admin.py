9b66dc37c6b57ba8e38f1cc987d34517d63bc231
Luis Del Giudice <luissierra@neoscloudllc.com>
2016-01-07 15:02:06 -0400

    Initial Commit

---
+++ almacen/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from almacen import models
+
+
+admin.site.register(models.Almacen)
+admin.site.register(models.Nodo)
+admin.site.register(models.Articulo)
+admin.site.register(models.Robot)
+admin.site.register(models.Viaje)
+admin.site.register(models.Nodo_Articulo)
